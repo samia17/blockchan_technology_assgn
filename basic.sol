7830cf30c35b61447476ad2d93f338ad35cb5c3f
SAMIA ANJUM <34435342+samia17@users.noreply.github.com>
2020-08-18 23:40:10 +0530

    Create basic.sol

---
+++ basic.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+ 
+
+contract cys {
+    
+    uint256 welcomemsg; 
+    string name; 
+    string password;
+    uint256 id;
+    int256 num;
+    
+   /** JAVA - visibility qualifier return-type function-name ()*/
+   /** solidity - function function-name()  visibility qualifier*/
+   
+   function setvalue(uint256 _msg, string memory _name, string memory _password, uint256 _id, int256 _no) public {
+       
+       welcomemsg = _msg;
+       name = _name;
+       password = _password;
+       id = _id;
+       num = _no;
+   }
+   
+   function getvalue() public view returns(uint256, string memory, string memory, uint256, int256) {
+       return (welcomemsg, name, password, id, num);
+   }
+    
+    
+}
